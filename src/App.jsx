f8ca70c066cc4985d00fd7c0258bcb353981d46b
AzkaFargh <azka.farghani.2001@gmail.com>
2024-12-09 11:54:20 +0700

    feat: add new nav item component

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,36 @@
-
-import './App.css'
+import logo from "./assets/logo.png";
+import NavItems from "./components/NavItems";
 
 function App() {
 
-
+  const menu = [
+    {
+      title: "Dashboard",
+      link: "#"
+    },
+    {
+      title: "Transfer",
+      link: "#"
+    },
+    {
+      title: "Topup",
+      link: "#"
+    },
+    {
+      title: "Signout",
+      link: "#"
+    },
+  ]
   return (
     <>
-    <p className="text-lg m-6 group relative w-max">
-     <span className='font-size: text-7xl'>Hwllo World</span>
-      <span className="absolute -bottom-1 left-1/2 w-0 transition-all h-0.5 bg-indigo-600 group-hover:w-3/6"></span>
-      <span className="absolute -bottom-1 right-1/2 w-0 transition-all h-0.5 bg-indigo-600 group-hover:w-3/6"></span>
-      </p>
+      <nav className="flex items-center w-screen justify-between bg-white py-4 px-8">
+        <img src={logo} />
+        <div className="flex gap-x-8">
+          <NavItems menu={menu} />
+        </div>
+      </nav>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
