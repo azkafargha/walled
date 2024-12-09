04f7982d85c9c47a804c946d29f6101ec1bcc8c3
AzkaFargh <azka.farghani.2001@gmail.com>
2024-12-09 11:14:24 +0700

    helow

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
     <p className="text-lg m-6 group relative w-max">
-     <span className='font-size: text-7xl'>Azka lop Azmi</span>
+     <span className='font-size: text-7xl'>Hwllo World</span>
       <span className="absolute -bottom-1 left-1/2 w-0 transition-all h-0.5 bg-indigo-600 group-hover:w-3/6"></span>
       <span className="absolute -bottom-1 right-1/2 w-0 transition-all h-0.5 bg-indigo-600 group-hover:w-3/6"></span>
       </p>
